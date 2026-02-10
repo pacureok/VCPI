8516ce218eaef6286dc57bf4cca4e1bc86eb987d
pacureok <okkpacur@gmail.com>
2026-02-09 23:12:33 -0600

    Update motor_vcpi.py

--- motor_vcpi.py
+++ motor_vcpi.py
@@ -1,25 +1,29 @@
-from direct.showbase.ShowBase import ShowBase
-from panda3d.core import LVector3, Fog, loadPrcFileData
+from panda3d.core import loadPrcFileData
+# ESTO ES VITAL: Configura el renderizado por software antes de importar ShowBase
+loadPrcFileData("", "window-type offscreen")
+loadPrcFileData("", "audio-library-name null")
+loadPrcFileData("", "gl-debug #t")
 
-class MotorVCPI:
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import Fog, LVector3
+
+class MotorVCPI(ShowBase):
     def __init__(self):
-        # Configuración para renderizar en la nube sin ventana
-        loadPrcFileData("", "window-type offscreen") 
-        self.base = ShowBase()
-        
+        ShowBase.__init__(self)
+
     def crear_escena(self, niebla_densidad=0.1):
-        # Crear Monolito
-        self.monolito = self.base.loader.loadModel("models/box")
-        self.monolito.setScale(1, 1, 3)
-        self.monolito.reparentTo(self.base.render)
+        self.render.getChildren().detach()
+        model = self.loader.loadModel("models/box")
+        model.reparentTo(self.render)
+        model.setPos(0, 10, 0)
+        model.setScale(1, 1, 3)
         
-        # Configurar Niebla
-        myFog = Fog("Fog VCPI")
-        myFog.setColor(0.5, 0.5, 0.5)
-        myFog.setExpDensity(niebla_densidad)
-        self.base.render.setFog(myFog)
+        # Niebla estilo Backrooms/Cinematic
+        fog = Fog("VCPI_Fog")
+        fog.setColor(0.1, 0.1, 0.1)
+        fog.setExpDensity(niebla_densidad)
+        self.render.setFog(fog)
         
-        # Renderizar un frame y guardarlo como imagen
-        self.base.graphicsEngine.renderFrame()
-        self.base.screenshot("render_vcpi.png")
+        self.graphicsEngine.renderFrame()
+        self.screenshot("render_vcpi.png", defaultFilename=False)
         return "render_vcpi.png"
