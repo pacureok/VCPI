3e196b041942bfa4fd76e57bafb9f4a9c166fef8
pacureok <okkpacur@gmail.com>
2026-02-10 00:18:23 -0600

    Update motor_vcpi.py

--- motor_vcpi.py
+++ motor_vcpi.py
@@ -2,7 +2,6 @@ from direct.showbase.ShowBase import ShowBase
 from panda3d.core import loadPrcFileData, AmbientLight, DirectionalLight, Vec4
 import os
 
-# Forzar renderizado "Offscreen"
 loadPrcFileData("", "window-type offscreen")
 loadPrcFileData("", "load-display p3tinydisplay")
 
@@ -11,29 +10,25 @@ class MotorVCPI(ShowBase):
         super().__init__()
         
     def crear_escena(self):
-        self.render.getChildren().detach() # Limpiar
+        self.render.getChildren().detach()
         
-        # Crear un objeto (Caja)
         box = self.loader.loadModel("models/box")
         box.reparentTo(self.render)
         box.setPos(0, 10, 0)
         box.setHpr(45, 45, 45)
         
-        # --- ILUMINACIÓN (Vital para que no sea negro) ---
-        # Luz Ambiental (luz general suave)
         alight = AmbientLight('alight')
-        alight.setColor(Vec4(0.4, 0.4, 0.4, 1))
+        alight.setColor(Vec4(0.5, 0.5, 0.5, 1))
         self.render.setLight(self.render.attachNewNode(alight))
         
-        # Luz Direccional (como el sol, crea sombras y volumen)
         dlight = DirectionalLight('dlight')
         dlight.setColor(Vec4(0.8, 0.8, 0.8, 1))
         dlnp = self.render.attachNewNode(dlight)
         dlnp.setHpr(0, -60, 0)
         self.render.setLight(dlnp)
         
-        # Captura
         self.graphicsEngine.renderFrame()
-        img_path = os.path.abspath("render_final.png")
+        img_name = "render_final.png"
+        img_path = os.path.join(os.getcwd(), img_name)
         self.screenshot(img_path, defaultFilename=False)
         return img_path
